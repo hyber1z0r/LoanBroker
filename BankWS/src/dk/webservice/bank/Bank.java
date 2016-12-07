286386d443907d8f03bdfa726b53b01e0e0f7ea4
hyber1z0r <jakobsky@live.dk>
2016-12-07 17:16:59 +0100

    feat(LoanBroker): Add Bank webservice
    A simple bank that will return a good interest rate for highly rated customers, and a less good
    interest rate for average to low rated customers

---
+++ BankWS/src/dk/webservice/bank/Bank.java
@@ -0,0 +1,15 @@
+package dk.webservice.bank;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService()
+public class Bank {
+    @WebMethod
+    public LoanResponse fileLoanRequest(String ssn, int creditScore, double loanAmount, long loanDuration) {
+        if (creditScore > 500) {
+            return new LoanResponse(2.5, ssn);
+        }
+        return new LoanResponse(4.0, ssn);
+    }
+}
